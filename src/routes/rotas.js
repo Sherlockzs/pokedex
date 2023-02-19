f549adc7bb343f0fa3580786b76b2406068182df
Gui Santos <gui.santos616@gmail.com>
2023-02-19 17:22:25 -0300

    first commit

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, HashRouter} from 'react-router-dom';
 import { Home } from '../pages/home/home';
 import { PokeInfo } from '../pages/pokeInfo/pokeInfo.jsx'
 
 export function Rotas() {
     return(
         <BrowserRouter>
-            <Routes>
-                <Route element = { <Home/> } path="/" />
-                <Route element = { <PokeInfo/> } path="/pokemon" />
-            </Routes>
+            <HashRouter>
+                <Routes>
+                    <Route element = { <Home/> } path="/home" />
+                    <Route element = { <PokeInfo/> } path="/pokemon" />
+                </Routes>
+            </HashRouter>
         </BrowserRouter>
     )
  }
